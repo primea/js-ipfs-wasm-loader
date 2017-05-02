6778ce015dd6996ea37f8918798e55a4fc3952b8
Silur <abrahamendre9@gmail.com>
2017-05-02 17:14:30 +0200

    restrict ipfs acces to only dag api

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const wasm2json = require('wasm-json-toolkit').wasm2json
 const CID = require('cids')
-const ipfs = new (require('ipfs'))()
 module.exports = class ipfsLoader {
-  static async loadImports (wasmCode) {
+
+  constructor(dagApi) {
+    this.dagApi = dagApi
+  }
+  async loadImports (wasmCode) {
     const ret = {}
     const importSection = this.getImportSection(wasmCode)
     for (let i = 0; i < importSection.length; i++) {
       const e = importSection[i]
       ret[e.moduleStr] = {code: Buffer.from([]), imports: {}}
 
-      if (this.isValidCID(e.moduleStr) && ipfs.isOnline()) {
+      if (this.isValidCID(e.moduleStr)) {
         ret[e.moduleStr].code = (await this.IpfsGet(e.moduleStr)).value
         ret[e.moduleStr].imports = this.getImportSection(ret[e.moduleStr].code)
       }
@@ -18,11 +21,12 @@ module.exports = class ipfsLoader {
     return ret
   }
 
-  static Instance (wasm, ipfsImports, localImports) {
+  Instance (wasm, ipfsImports, localImports) {
     ipfsImports = Object.assign(ipfsImports, localImports)
+    return ipfsImports
   }
 
-  static isValidCID (link) {
+  isValidCID (link) {
     try {
       CID.isCID(new CID(link))
     } catch (e) {
@@ -31,11 +35,11 @@ module.exports = class ipfsLoader {
     return true
   }
 
-  static IpfsGet (hash) {
-    return ipfs.dag.get(new CID(hash))
+  IpfsGet (hash) {
+    return this.dagApi.get(new CID(hash))
   }
 
-  static getImportSection (code) {
+  getImportSection (code) {
     code = wasm2json(code)
     return code.filter((e) => e.name === 'import')[0].entries
   }
