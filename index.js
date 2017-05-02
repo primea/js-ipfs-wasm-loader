114dfc911c067efe59464186fa79e87f9678b5a8
Silur <abrahamendre9@gmail.com>
2017-05-02 17:59:59 +0200

    make import resolving incremental-recursive

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ module.exports = class ipfsLoader {
   }
   async loadImports (wasmCode) {
     const ret = {}
-    const importSection = this.getImportSection(wasmCode)
+    let importSection = this.getImportSection(wasmCode)
     for (let i = 0; i < importSection.length; i++) {
       const e = importSection[i]
       ret[e.moduleStr] = {code: Buffer.from([]), imports: {}}
 
       if (this.isValidCID(e.moduleStr)) {
+        console.log('resolving', e.moduleStr)
         ret[e.moduleStr].code = (await this.IpfsGet(e.moduleStr)).value
         ret[e.moduleStr].imports = this.getImportSection(ret[e.moduleStr].code)
+        if(ret[e.moduleStr].imports == null) return ret
+        importSection = importSection.concat(ret[e.moduleStr].imports)
+        console.log('appended', importSection)
       }
     }
 
     return ret
   }
+
 
   Instance (wasm, ipfsImports, localImports) {
     ipfsImports = Object.assign(ipfsImports, localImports)
@@ -41,6 +46,8 @@ module.exports = class ipfsLoader {
 
   getImportSection (code) {
     code = wasm2json(code)
-    return code.filter((e) => e.name === 'import')[0].entries
+    const imports = code.filter((e) => e.name === 'import')[0]
+    if(!imports || !imports.entries) return null
+    return imports.entries
   }
 }
